2674d16df9f4843ee62b0c9252dc2ee5f8200e57
vpoal <vpoal@academian.com>
2025-08-04 15:13:59 +0530

    forth commit

--- src/datascience/components/model_evaluation.py
+++ src/datascience/components/model_evaluation.py
@@ -54,16 +54,17 @@ class ModelEvaluation:
             mlflow.log_metric("rmse", rmse)
             mlflow.log_metric("r2", r2)
             mlflow.log_metric("mae", mae)
+            mlflow.sklearn.log_model(model, "model")
 
 
-            # Model registry does not work with file store
-            if tracking_url_type_store != "file":
+            # # Model registry does not work with file store
+            # if tracking_url_type_store != "file":
 
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
-            else:
-                mlflow.sklearn.log_model(model, "model")
+            #     # Register the model
+            #     # There are other ways to use the Model Registry, which depends on the use case,
+            #     # please refer to the doc for more information:
+            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+            #     mlflow.sklearn.log_model(model, "model")
+            # else:
+            #     mlflow.sklearn.log_model(model, "model")
     
